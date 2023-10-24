d485fba72334491d3645916786a2b5e251be551e
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-10-24 13:06:15 -0600

    Add files via upload

--- dataset1/Recursión - Encontrar n números de la serie de Fibonacci.cpp
+++ dataset1/Recursión - Encontrar n números de la serie de Fibonacci.cpp
@@ -2,39 +2,44 @@
 
 #include <stdio.h>
 
-int fibonacci (int n)//Función para hacer la serie
+int fibonacci(int n) 
 {
-	if (n<0)
-		return 0;
-		
-	else if (n==1)
-		return 1;
-	
-	else 
-		return fibonacci(n-1)+fibonacci(n-2);	
+    if (n < 0)
+        return 0;
+        
+    else if (n==1)
+        return 1;
+        
+    else
+        return fibonacci(n-1)+fibonacci(n-2);
 }
 
-void variablesControl(int n, int i)//Función que sirve como variables de control
+void variablesControl(int n, int i)//Variables de control
 {
-    if (i < n)
-    {
-        printf("\n%d ", fibonacci(i));
-       	variablesControl(n, i + 1);
+    if (i<n) 
+	{
+        int serie = fibonacci(i);
+        if (serie >= 0)///Garantiza que los números no sean negativos 
+		{
+            printf("%d: %d\n", i + 1, serie);
+        }
+        variablesControl(n,i+1);
     }
 }
 
-int main()
+int main() 
 {
     int n;
     printf("Numero de elementos de la serie de Fibonacci: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("\n");
+
     printf("Serie de Fibonacci:\n");
     variablesControl(n,0);
-    printf("\n");
 
     return 0;
 }
+
 
 
 
